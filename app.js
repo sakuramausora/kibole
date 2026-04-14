3e68c14b9a42e32149845c2e08327962ba6be359
sakuramausora <75508551+sakuramausora@users.noreply.github.com>
2026-04-14 22:16:17 +0200

    Remove DOMContentLoaded event listener for navigation
    Removed event listener for DOMContentLoaded that manages active class for navigation links.

--- app.js
+++ app.js
@@ -143,21 +143,3 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll("nav a");
-  let currentPage = window.location.pathname.split("/").pop();
-
-  if (currentPage === "" || currentPage === "/") {
-    currentPage = "index.html";
-  }
-
-  links.forEach(link => {
-    const href = link.getAttribute("href");
-
-    if (href === currentPage) {
-      link.classList.add("active");
-    } else {
-      link.classList.remove("active");
-    }
-  });
-});
